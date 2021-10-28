3140b0cefe944688bf0d9807e4f7d167c00e2bfb
bharaths1513 <bharaths.1311@gmail.com>
2021-10-28 23:43:31 +0530

    updated: Employee wage per month

--- src/com/Emp_wage/Emp_Wage.java
+++ src/com/Emp_wage/Emp_Wage.java
@@ -6,6 +6,7 @@ class empwage
 	static int fulltime = 8;
 	static int parttime = 4;
 	static int rateperhr = 20;
+	static int workingdayspermonth = 20;
 	static int random = (int)(Math.random()*10)%3;
 	static int daily_wage;
 	void empattandance() 	//creating method for emp attendance
@@ -62,10 +63,34 @@ class empwage
 
 		}
 	}
-
-
-
-
+	
+	void emp_wage_for_month()
+	{
+		int emp_total_wage = 0;
+		for(int day=1; day<=20; day++)
+		{
+			int random1 = (int)(Math.random()*10)%3;
+			if(random1 == 0)
+			{
+				daily_wage = fulltime*rateperhr;
+				System.out.println("Employee is present for Full time");
+				System.out.println("Daily emp wage is "+daily_wage);
+			}
+			else if(random1 ==1)		
+			{
+				daily_wage = parttime*rateperhr;
+				System.out.println("Employee is present for Part time");
+				System.out.println("Daily employee wage is "+daily_wage);
+			}
+			else
+			{
+				daily_wage =0;
+				System.out.println("Daily emp wage is "+daily_wage);
+			}
+			emp_total_wage = emp_total_wage+daily_wage; 
+		}
+		System.out.println("Total employee wage of an employee per month "+emp_total_wage);
+	}
 }
 
 public class Emp_Wage {
@@ -73,7 +98,7 @@ public class Emp_Wage {
 		System.out.println("Welocome to Employee Wage compution Program");
 		empwage o=new empwage();
 		o.empattandance();
-		o.Switch_case();
+		o.emp_wage_for_month();
 
 	}
 }
