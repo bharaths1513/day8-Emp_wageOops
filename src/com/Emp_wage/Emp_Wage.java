cc03868ad3196e7f91aaf06df5e1d560eecaca75
bharaths1513 <bharaths.1311@gmail.com>
2021-10-28 23:03:29 +0530

    updated: Daily Employee Wage

--- src/com/Emp_wage/Emp_Wage.java
+++ src/com/Emp_wage/Emp_Wage.java
@@ -3,10 +3,12 @@ package com.Emp_wage;
 
 class empwage
 {
-
-	void empattandance()
+	static int fulltime =8;
+	static int rateperhr =20;
+	static int random = (int)(Math.random()*10)%2;
+	void empattandance() 	//creating method for emp attendance
 	{
-		int random = (int)(Math.random()*10)%2;
+
 		if(random == 0)
 		{
 			System.out.println("Employee is present");
@@ -17,6 +19,22 @@ class empwage
 		}
 	}
 
+	void Daily_emp_wage()	  //calculating daily empwage for full time
+	{
+		if(random == 0)
+		{
+			int daily_wage = fulltime*rateperhr;
+			System.out.println("Daily emp wage is "+daily_wage);
+		}
+		else
+		{
+			int daily_wage =0;
+			System.out.println("Daily emp wage is "+daily_wage);
+		}
+	}
+
+
+
 }
 
 public class Emp_Wage {
@@ -24,5 +42,7 @@ public class Emp_Wage {
 		System.out.println("Welocome to Employee Wage compution Program");
 		empwage o=new empwage();
 		o.empattandance();
+		o.Daily_emp_wage();
+
 	}
 }
