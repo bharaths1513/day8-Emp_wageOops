ec516869a6198e57f80a7654ff5c70d3e4d0952b
bharaths1513 <bharaths.1311@gmail.com>
2021-10-28 22:48:13 +0530

    updated: Employee attendance

--- src/com/Emp_wage/Emp_Wage.java
+++ src/com/Emp_wage/Emp_Wage.java
@@ -1,7 +1,28 @@
 package com.Emp_wage;
 
+
+class empwage
+{
+
+	void empattandance()
+	{
+		int random = (int)(Math.random()*10)%2;
+		if(random == 0)
+		{
+			System.out.println("Employee is present");
+		}
+		else
+		{
+			System.out.println("Employee is absent");
+		}
+	}
+
+}
+
 public class Emp_Wage {
 	public static void main(String[] args) {
 		System.out.println("Welocome to Employee Wage compution Program");
+		empwage o=new empwage();
+		o.empattandance();
 	}
 }
