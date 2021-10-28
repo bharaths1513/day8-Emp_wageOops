03d0fd2dd1087dbae2779ab15f0d25abb5b2816a
bharaths1513 <bharaths.1311@gmail.com>
2021-10-29 00:12:52 +0530

    updated: calculation for max 100hr and max 20 day per month

--- src/com/Emp_wage/Emp_Wage.java
+++ src/com/Emp_wage/Emp_Wage.java
@@ -6,9 +6,13 @@ class empwage
 	static int fulltime = 8;
 	static int parttime = 4;
 	static int rateperhr = 20;
-	static int workingdayspermonth = 20;
+	static int maxworkingdayspermonth = 20;
+	static int maxworkinghrpermonth = 100;
 	static int random = (int)(Math.random()*10)%3;
 	static int daily_wage;
+	static int totalworkinghrs;
+	static int totalworkingdays;
+	static int emphrs;
 	void empattandance() 	//creating method for emp attendance
 	{
 
@@ -63,14 +67,14 @@ class empwage
 
 		}
 	}
-	
-	void emp_wage_for_month()
+
+	void emp_wage_for_month()		//Calculcating employee wage per month 
 	{
 		int emp_total_wage = 0;
 		for(int day=1; day<=20; day++)
 		{
-			int random1 = (int)(Math.random()*10)%3;
-			if(random1 == 0)
+			int random1 = (int)(Math.random()*10)%3;	// This random is only for employee wage calculation per month 
+			if(random1 == 0)								
 			{
 				daily_wage = fulltime*rateperhr;
 				System.out.println("Employee is present for Full time");
@@ -91,6 +95,41 @@ class empwage
 		}
 		System.out.println("Total employee wage of an employee per month "+emp_total_wage);
 	}
+
+	void emp_wage_for_100hr()
+	{
+		while(totalworkinghrs<=maxworkinghrpermonth && totalworkingdays<=maxworkingdayspermonth)
+		{
+			int random1 = (int)(Math.random()*10)%3;
+			switch(random1)
+			{
+			case 0:
+				emphrs = 8;
+				System.out.println("Employee is present for Full time");
+				System.out.println("Daily emp wage is "+daily_wage);
+				break;
+			case 1:
+				emphrs = 4;
+				System.out.println("Employee is present for Part time");
+				System.out.println("Daily employee wage is "+daily_wage);
+				break;
+			case 2:
+				emphrs =0;
+				System.out.println("Daily emp wage is "+daily_wage);
+
+			}
+			if(totalworkinghrs<=100 )
+			{
+				totalworkinghrs = totalworkinghrs+emphrs;
+				System.out.println("Total hrs worked by employee in a month "+totalworkinghrs);
+				int totalempwage = totalworkinghrs*rateperhr;
+				System.out.println("Total wage earned by an employee in an month is "+totalempwage);
+				System.out.println("Total number day the employee worked "+totalworkingdays);
+				totalworkingdays++;
+			}
+			
+		}
+	}
 }
 
 public class Emp_Wage {
@@ -98,7 +137,7 @@ public class Emp_Wage {
 		System.out.println("Welocome to Employee Wage compution Program");
 		empwage o=new empwage();
 		o.empattandance();
-		o.emp_wage_for_month();
+		o.emp_wage_for_100hr();
 
 	}
 }
